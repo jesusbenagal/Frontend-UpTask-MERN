7243f92ebeb8d1d7376558d7c45d781d91bb1b24
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-06-15 15:58:13 +0200

    Creating Form

--- src/pages/NuevoProyecto.jsx
+++ src/pages/NuevoProyecto.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import FormularioProyecto from "../components/FormularioProyecto";
 
 const NuevoProyecto = () => {
   return (
     <>
-      <h1 className="text-4xl font-black">Nuevo Proyecto</h1>
+      <h1 className="text-4xl font-black">Crear Proyecto</h1>
 
-      <div></div>
+      <div className="mt-10 flex justify-center">
+        <FormularioProyecto />
+      </div>
     </>
   );
 };
