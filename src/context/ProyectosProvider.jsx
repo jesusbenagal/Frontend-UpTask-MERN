d330296b7ed0dc6f83a3ab8bca9ea732945fd978
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-06-18 10:05:42 +0200

    Finishing deleting projects

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -136,7 +136,34 @@ const ProyectosProvider = ({ children }) => {
   };
 
   const eliminarProyecto = async (id) => {
-    console.log("Eliminando", id);
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const { data } = await clienteAxios.delete(`/proyectos/${id}`, config);
+      // Sincronizar State
+      const proyectosActualizados = proyectos.filter(
+        (proyectoState) => proyectoState._id !== id
+      );
+      setProyectos(proyectosActualizados);
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+      setTimeout(() => {
+        setAlerta({});
+
+        navigate("/proyectos");
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
