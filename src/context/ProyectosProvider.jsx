2735216c0fa97d3bdbb2085d5a837f8bd5994cc9
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-06-20 23:19:56 +0200

    Adding Modal to Context

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -9,6 +9,7 @@ const ProyectosProvider = ({ children }) => {
   const [alerta, setAlerta] = useState({});
   const [proyecto, setProyecto] = useState({});
   const [cargando, setCargando] = useState(false);
+  const [modalFormularioTarea, setModalFormularioTarea] = useState(false);
 
   const navigate = useNavigate();
 
@@ -166,6 +167,10 @@ const ProyectosProvider = ({ children }) => {
     }
   };
 
+  const handleModalTarea = () => {
+    setModalFormularioTarea(!modalFormularioTarea);
+  };
+
   return (
     <ProyectosContext.Provider
       value={{
@@ -177,6 +182,8 @@ const ProyectosProvider = ({ children }) => {
         proyecto,
         cargando,
         eliminarProyecto,
+        modalFormularioTarea,
+        handleModalTarea,
       }}
     >
       {children}
--- src/pages/Proyecto.jsx
+++ src/pages/Proyecto.jsx
@@ -5,7 +5,8 @@ import ModalFormularioTarea from "../components/ModalFormularioTarea";
 
 const Proyecto = () => {
   const params = useParams();
-  const { obtenerProyecto, proyecto, cargando } = useProyectos();
+  const { obtenerProyecto, proyecto, cargando, handleModalTarea } =
+    useProyectos();
 
   const [modal, setModal] = useState(false);
 
@@ -45,7 +46,7 @@ const Proyecto = () => {
         </div>
       </div>
       <button
-        onClick={() => setModal(true)}
+        onClick={handleModalTarea}
         type="button"
         className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center"
       >
