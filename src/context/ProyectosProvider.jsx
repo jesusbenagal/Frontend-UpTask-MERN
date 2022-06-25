7d4971e72ab2738cff9001b6734b659b6e500d25
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-06-25 14:09:57 +0200

    Adding task to State and formatting form

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -184,7 +184,12 @@ const ProyectosProvider = ({ children }) => {
       };
 
       const { data } = await clienteAxios.post("/tareas", tarea, config);
-      console.log(data);
+      //Agregar la tarea al State
+      const proyectoActualizado = { ...proyecto };
+      proyectoActualizado.tareas = [...proyecto.tareas, data];
+      setProyecto(proyectoActualizado);
+      setAlerta({});
+      setModalFormularioTarea(false);
     } catch (error) {
       console.log(error);
     }
