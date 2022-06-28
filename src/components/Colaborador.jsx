a65a001f322566538647d66d979fa0be5ea1be9f
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-06-28 21:55:55 +0200

    Styling Colaboradores Component

--- src/components/Colaborador.jsx
+++ src/components/Colaborador.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Colaborador = () => {
-  return <div>Colaborador</div>;
+const Colaborador = ({ colaborador }) => {
+  const { nombre, email } = colaborador;
+  return (
+    <div className="border-b p-5 flex justify-between items-center">
+      <div>
+        <p>{nombre}</p>
+        <p className="text-sm text-gray-700">{email}</p>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg hover:bg-red-700 transition-colors"
+        >
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Colaborador;
